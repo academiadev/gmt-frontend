b6f89ff62a0496e95db62eb0e12b87a1dbae10e5
Gabriel Duarte <hoepersduarte@gmail.com>
2018-06-09 16:13:08 -0300

    add login and auth service
    Adicionado a funcionalidade de login com acesso por token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule, MatInputModule } from '@angular/material';
 import { DashComponent } from './dash/dash.component';
-import { TableComponent } from './table/table.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { CompanyComponent } from './company/company.component';
+import { CompanyEditComponent } from './company-edit/company-edit.component';
+import { AppErrorHandler } from './commons/app-error-handler';
+import { AuthGuard } from './service/auth-guard.service';
+import { AuthService } from './service/auth.service';
+import { CompanyService } from './service/company.service';
+import { ToastrModule } from 'ngx-toastr';
+import { JwtModule } from '@auth0/angular-jwt';
+import { environment } from '../environments/environment';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+export function tokenGetter() {
+  const token = localStorage.getItem(environment.tokenName);
+  return token;
+}
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     DashComponent,
-    TableComponent
+    NotFoundComponent,
+    LoginComponent,
+    NavigationComponent,
+    CompanyComponent,
+    CompanyEditComponent
   ],
   imports: [
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FlexLayoutModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
@@ -32,9 +60,27 @@ import { TableComponent } from './table/table.component';
     MatMenuModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        whitelistedDomains: [
+          environment.backEndUrl
+        ],
+        blacklistedRoutes: [
+          environment.urls.auth.url
+        ]
+      }
+    }),
+    ToastrModule.forRoot(),
   ],
-  providers: [],
+  providers: [
+    CompanyService,
+    AuthService,
+    AppErrorHandler,
+    AuthGuard,
+    { provide: ErrorHandler, useClass: AppErrorHandler }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  backEndUrl: 'http://localhost:8080',
+  tokenName: 'access_token',
+  urls: {
+    auth: {
+      url: 'http://localhost:8080/auth',
+      login: 'http://localhost:8080/auth/login',
+      refresh: 'http://localhost:8080/auth/refresh',
+    },
+    company: {
+      url: 'http://localhost:8080/company'
+    }
+  }
 };
 
 /*
