6cc4188d6ca0e5520772da3fed8d6ed415de736f
tiaagosr <tiago.schoeping@gmail.com>
2018-06-16 17:21:44 -0300

    Added request password and change password pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,24 @@ import { CompanyEditComponent } from './pages/company-edit/company-edit.componen
 import { DashComponent } from './pages/dash/dash.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
 import { AuthGuard } from './service/auth-guard.service';
 import { LoginGuard } from './service/login-guard.service';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RequestPasswordComponent } from './pages/request-password/request-password.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
-  { path: 'registrar', component: CreateUserComponent },
+  { path: 'registrar', component: CreateUserComponent, canActivate: [LoginGuard],
+    children: [
+      { path: ':token', component: CreateUserComponent } 
+    ] 
+  },
+  { path: 'trocar-senha', component: RequestPasswordComponent, canActivate: [LoginGuard] },
+  //{ path: 'trocar-senha/:codigo', component: NewPasswordComponent, canActivate: [LoginGuard] },
   { path: 'not-found', component: NotFoundComponent },
   {
     path: '', component: NavigationComponent, canActivate: [AuthGuard],
