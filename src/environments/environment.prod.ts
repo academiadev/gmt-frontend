332e1e017f2ac00bb22f21ad7191c8d891bd10a8
tiaagosr <tiago.schoeping@gmail.com>
2018-06-24 09:12:57 -0300

    Updated backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production: true,
-  backEndUrl: 'https://powerful-retreat-98533.herokuapp.com',
+  backEndUrl: 'https://bluerefundgmt.herokuapp.com',
   tokenName: 'access_token',
   urls: {
     auth: {
-      url: 'https://powerful-retreat-98533.herokuapp.com/auth',
-      login: 'https://powerful-retreat-98533.herokuapp.com/auth/login',
-      refresh: 'https://powerful-retreat-98533.herokuapp.com/auth/refresh',
+      url: 'https://bluerefundgmt.herokuapp.com/auth',
+      login: 'https://bluerefundgmt.herokuapp.com/auth/login',
+      refresh: 'https://bluerefundgmt.herokuapp.com/auth/refresh',
     },
     company: {
-      url: 'https://powerful-retreat-98533.herokuapp.com/company'
+      url: 'https://bluerefundgmt.herokuapp.com/company'
     }
   }
 };
